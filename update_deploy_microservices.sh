ece4fdbe00efebfc38a79eb758ecd5a3027996bd
Diksha Munjal <57708209+Dikshammunjal@users.noreply.github.com>
2022-08-26 15:18:49 +0530

    Update update_deploy_microservices.sh

--- update_deploy_microservices.sh
+++ update_deploy_microservices.sh
@@ -18,7 +18,7 @@ kubectl create ns $changens
 kubectl create secret docker-registry ocirloginsecret --docker-server=$DOCKERURL  --docker-username=''$USERNAME'' --docker-password=''$PASSWORD''  --docker-email=''$EMAIL'' -n $changens
 
 
-cp deployment_template.yaml "$changedeploymentname"_deployment_template.yaml
+cp /u01/shared/scripts/pipeline/microservices/base_oke_template_jenkins/deployment_template.yaml "$changedeploymentname"_deployment_template.yaml
 
  /usr/bin/sed -i -e 's%changens%'$changens'%g' -e 's%changedeploymentname%'$changedeploymentname'%g' -e 's%changeimage%'$changeimage'%g' -e 's%changeport%'$changeport'%g' -e 's%changeservice%'$changeservice'%g' "$changedeploymentname"_deployment_template.yaml
 
