b8ecf7431544e03b6085f13d16d7654d9e56f8af
Diksha Munjal <57708209+Dikshammunjal@users.noreply.github.com>
2022-09-12 13:22:14 +0530

    Update pushimage.sh

--- pushimage.sh
+++ pushimage.sh
@@ -7,10 +7,9 @@ MS=$4
 GITCOMMIT=$5
 IMAGE=$6
 
-export local_REGISTRY_TOKEN=echo ''$PASSWORD'' | sed 's/[!@#$%^&*()-]/\\\&/g'
+export local_REGISTRY_TOKEN=`echo $PASSWORD | sed 's/[!@#$%^&*()-]/\\\&/g'`
 
-docker login -u ''$USERNAME'' -p echo ''${local_REGISTRY_TOKEN}'' ''$REGION''.ocir.io"
-
+docker login -u ''$USERNAME'' -p ''${local_REGISTRY_TOKEN}'' ''$REGION''.ocir.io
 
 docker tag ''$MS'':''$GITCOMMIT'' ''$IMAGE''
 
